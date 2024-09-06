cf96d95d55245d3300feff166f0c0b3358755920
Thykp <101046365+Thykp@users.noreply.github.com>
2024-09-06 16:42:35 +0800

    updated theme

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -11,22 +11,21 @@ import { initializeApp } from "firebase/app";
 
 const MyPreset = definePreset(Aura, {
   semantic: {
-      primary: {
-          50: '{indigo.50}',
-          100: '{indigo.100}',
-          200: '{indigo.200}',
-          300: '{indigo.300}',
-          400: '{indigo.400}',
-          500: '{indigo.500}',
-          600: '{indigo.600}',
-          700: '{indigo.700}',
-          800: '{indigo.800}',
-          900: '{indigo.900}',
-          950: '{indigo.950}'
-      }
+    primary: {
+      50: '#e3f2fd',
+      100: '#bbdefb',
+      200: '#90caf9',
+      300: '#64b5f6',
+      400: '#42a5f5',
+      500: '#2196f3',
+      600: '#1e88e5',
+      700: '#1976d2',
+      800: '#1565c0',
+      900: '#0d47a1',
+      950: '#0b3d91'
+    }
   }
 });
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
