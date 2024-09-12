609bdc7fd2ecc284822d9d364fd4dad849304746
Thykp <101046365+Thykp@users.noreply.github.com>
2024-09-12 23:50:49 +0800

    fixed routing after auth

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -3,10 +3,10 @@ import { createRouter, createWebHistory } from "vue-router";
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/", component: () => import("../views/Home.vue")},
+        { path: "/", component: () => import("../views/Landing.vue")},
         { path: "/login", component: () => import("../views/Login.vue")},
         { path: "/register", component: () => import("../views/Register.vue")},
-        
+        { path: "/home", component: () => import("../views/Home.vue")},
     ],
 });
 
