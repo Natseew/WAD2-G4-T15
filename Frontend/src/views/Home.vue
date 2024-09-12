56526e40dc80e6252bee7ba284e0669d3bcb7ade
Natseew <natseew@gmail.com>
2024-09-12 16:06:51 +0800

    default home, redirect to login if user not logged in

--- Frontend/src/views/Home.vue
+++ Frontend/src/views/Home.vue
@@ -2,7 +2,24 @@
   <h1>User signed in</h1>
 </template>
   
-<script>
+<script setup>
+  import { ref } from 'vue';
+  import { getAuth, onAuthStateChanged } from "firebase/auth";
+  import { useRouter } from 'vue-router';
+  
+  const router = useRouter();
+  
+
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+      // User is signed in
+      console.log(user)
+      } else {
+        // User is signed out
+        router.push('/login');
+      }
+    });
 
 </script>
 
--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 Backend:
 1. cd Backend
-2. npm run start
+2. npm install
+3. npm run start
 
 
 Frontend:
 1. cd Frontend
-2. npm run dev
+2. npm install
+3. npm run dev
