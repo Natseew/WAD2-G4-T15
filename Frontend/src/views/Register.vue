eb36fe3d15af1894af94a09515c1fef0814c1ea2
LorenzFar <lorenzof.w.2023@scis.smu.edu.sg>
2024-09-27 23:17:36 +0800

    Update Register.vue

--- Frontend/src/views/Register.vue
+++ Frontend/src/views/Register.vue
@@ -1,8 +1,5 @@
 <template>
-    <div class="register-container">
-        <div class="logo"> 
-          Dating App. 
-        </div>
+    <div class="register-container w-full lg:h-full lg:items-center lg:grid grid-cols-2 xl:gap-64 lg:gap-8">
         <div class="register-form">
           <h1 class=text-3xl>Create an Account</h1>
           <p>
@@ -22,6 +19,9 @@
             <router-link to="/login">Login</router-link>
           </p>
           <p v-if="errMsg" class="error-message">{{ errMsg }}</p>
+        </div>
+        <div class="logo"> 
+          Dating App. 
         </div>
     </div>
   </template>
@@ -64,46 +64,88 @@
   </script>
   
   <style scoped>
+
+  @media (max-width: 1023px) {
+    .logo{
+      position: absolute;
+      display: flex;
+      top: 20px;
+      left: 20px;
+      font-size: 20px;
+      font-family: Roboto Flex;
+      color: white;
+    }
+
+    .register-container {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100%;
+      width: 100%;
+      padding: 2rem;
+      box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
+      border-radius: 8px;
+      background: linear-gradient(180deg, #FF7676 0%, #F54EA2 100%);
+    }
+    .register-form {
+      width: 100%;
+      max-width: 400px;
+      box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1);
+      padding: 70px 30px 40px 30px;
+      border-radius: 25px;
+      background: rgba(255, 255, 255, 0.8);
+      backdrop-filter: blur(10px);
+    }
+  }
+
+  @media (min-width: 1024px) {
   .register-container {
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-    height: 100%;
+    height: 100vh;
     width: 100%;
     padding: 2rem;
     box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.1);
     border-radius: 8px;
-    background: linear-gradient(180deg, #FF7676 0%, #F54EA2 100%);
+    background: #fff;
   }
-  
-  h1 {
-    text-align: center;
-    margin-bottom: 2rem;
-  }
-  
+
   .register-form {
     width: 100%;
     max-width: 400px;
+    max-height: 600px;
     box-shadow: 0 4px 16px rgba(0, 0, 0, 0.1);
     padding: 70px 30px 40px 30px;
     border-radius: 25px;
     background: rgba(255, 255, 255, 0.8);
     backdrop-filter: blur(10px);
   }
+
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    max-width: 800px;
+    background: linear-gradient(180deg, #FF7676 0%, #F54EA2 100%);
+    font-size: 3rem;
+    color: white;
+    text-align: center;
+    font-family: 'Roboto Flex', sans-serif;
+  }
+}
+  
+  h1 {
+    text-align: center;
+    margin-bottom: 2rem;
+  }
+
   
   .register-form p {
     margin-bottom: 1rem;
-  }
-
-  .logo{
-    position: absolute;
-    display: flex;
-    top: 20px;
-    left: 20px;
-    font-size: 20px;
-    font-family: Roboto Flex;
-    color: white;
   }
 
   :deep .p-inputtext,
