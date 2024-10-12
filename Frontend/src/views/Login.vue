0d425affeb4897725d9a16f18cf3cb10278786a4
LorenzFar <lorenzof.w.2023@scis.smu.edu.sg>
2024-10-12 13:28:54 +0800

    Update Login.vue

--- Frontend/src/views/Login.vue
+++ Frontend/src/views/Login.vue
@@ -10,7 +10,7 @@
             <Password v-model="password" toggleMask placeholder="Password" feedback={false} />
           </p>
           <p>
-            <Button label="Register" icon="pi pi-check" class="p-button-rounded w-48 md:w-2/3" @click="register" />
+            <Button label="Login" icon="pi pi-check" class="p-button-rounded w-48 md:w-2/3" @click="register" />
           </p>
           <p>Don't have an account? 
           <router-link to="/register">Register Now</router-link>
