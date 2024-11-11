d429a1cc53fb659f98ffcc0abd596cf4abd4dc6f
natseew <natseew@gmail.com>
2024-11-11 17:57:58 +0800

    change background colour

--- Frontend/src/views/gallery/gallery.vue
+++ Frontend/src/views/gallery/gallery.vue
@@ -1,7 +1,8 @@
 <template>
     <div id="app" style="overflow:scroll">
       <Navbar class="navbar" />
-      <PhotoGalleryPage class="mt-15 mb-15" />
+      <PhotoGalleryPage class="mt-15 mb-15 z-2" />
+      <div class="background"></div>
     </div>
   </template>
   
@@ -21,5 +22,15 @@
 <style scoped>
 .navbar {
   z-index: 10;
+}
+
+.background {
+  background: linear-gradient(180deg, #ffa578, rgb(255, 128, 192), #a67bf5);
+  animation: spin-gradient 15s ease infinite;
+  height: 100vh;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 </style>
