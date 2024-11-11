29a2ed8e1ee22a3671d5bd3dc59a29e0effce17c
Thykp <101046365+Thykp@users.noreply.github.com>
2024-11-11 22:14:05 +0800

    bug fix for router error

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -8,7 +8,6 @@ const router = createRouter({
         { path: "/login", component: () => import("../views/Login.vue")},
         { path: "/register", component: () => import("../views/Register.vue")},
         { path: "/profile", component: () => import("../views/profile/Profile.vue")},
-        { path: "/editProfile", component: () => import("../views/profile/CardEdit.vue")},
         { path: "/chat", component: () => import("../views/chat.vue")},
         { path: "/quiz", component: () => import("../views/Quiz.vue")},
         { path: "/matches", component: () => import("../views/Matches.vue")},
